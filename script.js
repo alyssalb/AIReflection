25f44191082c211c2e6dcb87b9d98a4636511bbb
alyssalb <alyssa.barrack@ucf.edu>
2024-12-01 15:46:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var coll = document.getElementsByClassName('collapsible-button');
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function() {
+      // Toggle active class on the button
+      this.classList.toggle('active');
+
+      // Toggle between hiding and showing the collapsible content
+      var content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+        content.style.display = 'none';
+      } else {
+        content.style.display = 'block';
+      }
+    });
+  }
+});
