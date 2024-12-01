67946f37f7b3227b5b97c1aed44bceb26838b19d
alyssalb <alyssa.barrack@ucf.edu>
2024-12-01 15:53:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var iframe = document.getElementById('portfolio-iframe');
-  var fullscreenBtn = document.getElementById('fullscreen-btn');
-
-  fullscreenBtn.addEventListener('click', function() {
-    // Check if the browser supports the Fullscreen API
-    if (iframe.requestFullscreen) {
-      iframe.requestFullscreen();
-    } else if (iframe.webkitRequestFullscreen) { /* Safari */
-      iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) { /* IE11 */
-      iframe.msRequestFullscreen();
-    } else {
-      alert('Your browser does not support fullscreen mode.');
-    }
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
   var coll = document.getElementsByClassName('collapsible-button');
   var i;
 
